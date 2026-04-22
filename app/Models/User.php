3eefd69f6fbe4ee877a66ad6bc4b20b60536a186
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-22 17:14:44 -0600

    validator users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 
 #[Fillable(['name', 'username', 'password', 'is_admin', 'must_change_password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -18,10 +18,25 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+        * Campos permitidos para mass assignment
+    */
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'is_admin',
+        'must_change_password'
+    ];
+
+
+    /**
+        * Campos ocultos en serializaciones
+    */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     protected function casts(): array
     {
         return [
