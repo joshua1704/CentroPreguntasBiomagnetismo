def3d8a66068fc741eb4bc8f910093d6b4f24cf2
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-22 17:26:36 -0600

    validator

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\TopicController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
@@ -26,19 +25,17 @@ Route::prefix('admin')->name('admin_')->group(function() {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
-
     Route::get('/change_password', [AuthController::class, 'showChangePassword']);
     Route::post('/change_password', [AuthController::class, 'changePassword']);
 
-    //Route::middleware(['auth', 'force.password'])->group(function() {
+    Route::middleware(['auth', 'force.password'])->group(function() {
         Route::get('/preguntas/{sidebar}', [QuestionController::class, 'getQuestions'])->name('get_questions');
         Route::get('/editar/pregunta/{id}/{sidebar}', [QuestionController::class, 'showEdit'])->name('edit_question');
         Route::get('/cambia/status/pregunta/{id}/{status}', [QuestionController::class, 'changeStatusQuestion'])->name('change_status_question');
         Route::get('/users', [UserController::class, 'index'])->name('users');
         Route::post('/user/store', [UserController::class, 'store'])->name('user_store');
         Route::get('/topics', [TopicController::class, 'index'])->name('topics');
-
         Route::post('/pregunta/modificar', [QuestionController::class, 'update'])->name('update_question');
         Route::post('/upload_image', [QuestionController::class, 'uploadImage']);
-    //});
+    });
 });
