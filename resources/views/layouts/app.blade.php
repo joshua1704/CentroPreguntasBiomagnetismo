e5138f2b5b547f4d54159ef3ab7e7704d357d7c7
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-22 16:30:28 -0600

    add script url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/build/assets/app-BTNiwQj5.css">
-    <script src="/build/assets/app-C3ShDR98.js"></script>
+    <script src="/build/assets/app-C3ShDR9B.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/quill@2.0.0/dist/quill.snow.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/quill@2.0.0/dist/quill.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
