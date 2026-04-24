7f5e3f98551e51363edc56d204038f8028b7d8cc
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-24 14:32:10 -0600

    all changes

--- resources/views/admin/layouts/formFieldsQuestions.blade.php
+++ resources/views/admin/layouts/formFieldsQuestions.blade.php
@@ -1,54 +1,42 @@
-<div class="d-none d-md-block">
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-3 mb-3 mb-lg-0">
-            <div class="d-flex d-lg-block align-items-center gap-2">
-                <select class="form-select form-select-sm" id="inputSelectTopics" name="topic">
-                    <option value="">Todos los temas</option>
-                    @foreach ($topics as $topic)
-                        <option value="{{ $topic->id }}" {{ ($search_params['topic'] ?? '') == $topic->id ? 'selected' : '' }}>{{ $topic->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 mb-3 mb-3 mb-lg-0">
-            <div class="d-flex d-lg-block align-items-center gap-2">
-                <select class="form-select form-select-sm {{ ($search_params['date'] ?? '') == 'custome_range' ? 'd-none' : '' }}" id="inputSelectDate" name="date">
-                    <option value="">Todas las fechas</option>
-                    <option value="today" {{ ($search_params['date'] ?? '') == 'today' ? 'selected' : '' }}>Hoy</option>
-                    <option value="week" {{ ($search_params['date'] ?? '') == 'week' ? 'selected' : '' }}>Una semana</option>
-                    <option value="month" {{ ($search_params['date'] ?? '') == 'month' ? 'selected' : '' }}>Un mes</option>
-                    <option value="last_month" {{ ($search_params['date'] ?? '') == 'last_month' ? 'selected' : '' }}>Mes pasado</option>
-                    <option value="custome_range" {{ ($search_params['date'] ?? '') == 'custome_range' ? 'selected' : '' }}>Rango personalizado</option>
-                </select>
-                <div id="dateRange" class="input-group {{ ($search_params['date'] ?? '') == 'custome_range' ? '' : 'd-none' }}">
-                    <input type="date" id="desdeDate" name="desdeDate" class="form-control form-control-sm" max="{{ date('Y-m-d') }}" value="{{ ($search_params['desdeDate'] ?? '') }}">
-                    <input type="date" id="hastaDate" name="hastaDate" class="form-control form-control-sm" max="{{ date('Y-m-d') }}" min="{{ $search_params['desdeDate'] ?? '' }}" value="{{ $search_params['hastaDate'] ?? '' }}">
-                    <button type="button" id="cancelDate" class="btn btn-danger btn-sm">X</button>
-                </div>
-
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-3 mb-3 mb-lg-0">
-            <div class="d-flex d-lg-block align-items-center gap-2">
-                <input class="form-control form-control-sm" type="search" placeholder="Texto a buscar" name="search" value="{{ $search_params['search'] ?? '' }}">
-            </div>
-        </div>
-        <div class="offset-3 col-6 offset-md-0 col-md-6 offset-lg-0 col-lg-2 mb-3 mb-lg-0 align-content-end">
-            <button class="btn btn-primary btn-sm w-100" id="btnSearchParamsSubmit">Buscar</button>
-            <button type="button" class="btn btn-primary btn-sm w-100 d-none" id="btnSearchParamsSpinner" disabled>
-                <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
-                <span role="status">Buscando...</span>
-            </button>
+<div class="row">
+    <div class="col-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+        <div class="d-flex d-lg-block align-items-center gap-2">
+            <select class="form-select form-select-sm" id="inputSelectTopics" name="topic">
+                <option value="">Todos los temas</option>
+                @foreach ($topics as $topic)
+                    <option value="{{ $topic->id }}" {{ ($search_params['topic'] ?? '') == $topic->id ? 'selected' : '' }}>{{ $topic->name }}</option>
+                @endforeach
+            </select>
         </div>
     </div>
-</div>
-<div class="d-flex d-md-none justify-content-center">
-    <button type="button" class="btn btn-outline-secondary btn-sm" id="btnOpenFormFieldsQuestions">
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-        </svg>
-    </button>
-</div>
-<script>
+    <div class="col-6 col-md-6 col-lg-4 mb-3 mb-3 mb-lg-0">
+        <div class="d-flex d-lg-block align-items-center gap-2">
+            <select class="form-select form-select-sm {{ ($search_params['date'] ?? '') == 'custome_range' ? 'd-none' : '' }}" id="inputSelectDate" name="date">
+                <option value="">Todas las fechas</option>
+                <option value="today" {{ ($search_params['date'] ?? '') == 'today' ? 'selected' : '' }}>Hoy</option>
+                <option value="week" {{ ($search_params['date'] ?? '') == 'week' ? 'selected' : '' }}>Una semana</option>
+                <option value="month" {{ ($search_params['date'] ?? '') == 'month' ? 'selected' : '' }}>Un mes</option>
+                <option value="last_month" {{ ($search_params['date'] ?? '') == 'last_month' ? 'selected' : '' }}>Mes pasado</option>
+                <option value="custome_range" {{ ($search_params['date'] ?? '') == 'custome_range' ? 'selected' : '' }}>Rango personalizado</option>
+            </select>
+            <div id="dateRange" class="input-group {{ ($search_params['date'] ?? '') == 'custome_range' ? '' : 'd-none' }}">
+                <input type="date" id="desdeDate" name="desdeDate" class="form-control form-control-sm" max="{{ date('Y-m-d') }}" value="{{ ($search_params['desdeDate'] ?? '') }}">
+                <input type="date" id="hastaDate" name="hastaDate" class="form-control form-control-sm" max="{{ date('Y-m-d') }}" min="{{ $search_params['desdeDate'] ?? '' }}" value="{{ $search_params['hastaDate'] ?? '' }}">
+                <button type="button" id="cancelDate" class="btn btn-danger btn-sm">X</button>
+            </div>
 
-</script>
+        </div>
+    </div>
+    <div class="col-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+        <div class="d-flex d-lg-block align-items-center gap-2">
+            <input class="form-control form-control-sm" type="search" placeholder="Texto a buscar" name="search" value="{{ $search_params['search'] ?? '' }}">
+        </div>
+    </div>
+    <div class="offset-0 col-6 offset-md-0 col-md-6 offset-lg-0 col-lg-2 mb-3 mb-lg-0 align-content-end">
+        <button class="btn btn-primary btn-sm w-100" id="btnSearchParamsSubmit">Buscar</button>
+        <button type="button" class="btn btn-primary btn-sm w-100 d-none" id="btnSearchParamsSpinner" disabled>
+            <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+            <span role="status">Buscando...</span>
+        </button>
+    </div>
+</div>
