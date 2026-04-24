37392ff614f95d90e88028afec2fb5069b3f96b6
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-24 13:47:03 -0600

    all changes

--- resources/views/admin/layouts/formFieldsQuestions.blade.php
+++ resources/views/admin/layouts/formFieldsQuestions.blade.php
@@ -44,11 +44,13 @@
         </button>
     </div>
 </div>
-<button type="button" class="btn btn-outline-primary btn-sm d-block d-md-none" id="btnOpenFormFieldsQuestions">
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-    </svg>
-</button>
+<div class="d-flex d-md-none align-items-end">
+    <button type="button" class="btn btn-outline-primary btn-sm" id="btnOpenFormFieldsQuestions">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+        </svg>
+    </button>
+</div>
 <script>
 
 </script>
