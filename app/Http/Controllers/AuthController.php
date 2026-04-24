864ba45b7a1e2bde31c35df4d6d79711bb681cbb
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-23 19:07:09 -0600

    all changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect()->route('admin_login');
     }
 
     public function showChangePassword()
