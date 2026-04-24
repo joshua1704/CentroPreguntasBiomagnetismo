044ca5db844994acaa1be1e2942df38a74f39382
Josué Emmanuel Rodríguez Velazco <jerodriguez@syd.com.mx>
2026-04-23 19:48:19 -0600

    all changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,6 +34,7 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request) {
+        dd('logout');
         Auth::logout();
 
         $request->session()->invalidate();
